1ec90a46139aeafcbeb34f89202957cb21edd289
taufiqqr <148629011+taufiqqr@users.noreply.github.com>
2023-10-29 20:22:52 +0700

    Update latihan1_variabel.py

--- latihan1_variabel.py
+++ latihan1_variabel.py
@@ -1,4 +1,4 @@
-1.
+
 a = 30
 b = 20
 print("a :",a)
@@ -6,18 +6,18 @@ print("b :",b)
 print("hasil a+b =",a+b)
 print("hasil a-b =",a-b)
 
-2.
+
 a = 30
 b = 20
 c = 5
 nilai1 = a*b
 nilai2 = a+c
-print("a : a",a)
-print("b : a",b)
-print("c : c",c)
+print("a : ",a)
+print("b : ",b)
+print("c : ",c)
 print(nilai1/nilai2)
 
-3.
+
 a = 10
 b = 8
 t = 5
@@ -27,14 +27,14 @@ print("b :",b)
 print("t :",t)
 print("luas trapesium : 1/2 x (a+b) x t, hasil: ",0.5*nilai*t)
 
-4.
+
 r = 14
 pi = 3.14
 print("r :",r)
 print("pi :",pi)
 print("rumus keliling lingkaran: 2 x pi x r, hasil :",2*pi*r)
 
-5.
+
 r = 7
 pi = 3.14
 print("r : ",r)
