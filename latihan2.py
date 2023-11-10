bcb393afd3ea47166e465252477fd417ae557e9c
taufiqqr <148629011+taufiqqr@users.noreply.github.com>
2023-11-10 21:20:59 +0700

    Create latihan2.py

---
+++ latihan2.py
@@ -0,0 +1,59 @@
+a = int(input("masukkan nilai a:"))
+b = int(input("masukkan nilai b:"))
+c = int(input("masukkan nilai c:"))
+nilai = (a + b) * (b - c)
+print("===========================")
+print("nilai a: ",a)
+print("nilai b: ",b)
+print("nilai c: ",c)
+print("===========================")
+print("hasil dari (a+b)*(a-c):",nilai)
+print("===========================")
+
+
+a = int(input("masukkan nilai a:"))
+b = int(input("masukkan nilai b:"))
+c = int(input("masukkan nilai c:"))
+nilai = (a * b) / (a + c)
+print("===========================")
+print("nilai a: ",a)
+print("nilai b: ",b)
+print("nilai c: ",c)
+print("===========================")
+print("hasil dari (axb)/(a+c):",nilai)
+print("===========================")
+
+
+a = int(input("masukkan nilai a:"))
+b = int(input("maaukkan nilai b:"))
+t = int(input("maaukkan nilai t:"))
+hasil = 0.5 * (a+b) * t
+cm = hasil * 100
+print("____________________________")
+print("a :",a,"m")
+print("b :",b,"m")
+print("t :",t,"m")
+print("____________________________")
+print("luas trapesium : 1/2 x (a+b) x t, hasil= ",hasil,"m")
+print("= ",cm,"cm")
+print("____________________________")
+
+
+r = int(input("masukkan nilai jari-jari:"))
+pi = 3.14
+print("-----------------------------------")
+print("r = ",r,"cm")
+print("pi = 3,14")
+print("-----------------------------------")
+print("rumus keliling lingkaran: 2 x pi x r, hasil :",2*pi*r,"cm")
+print("-----------------------------------")
+
+
+r = int(input("masukkan jari-jari = "))
+pi = 3.14
+print("=======================")
+print("r : ",r,"cm")
+print("pi : ",pi)
+print("=======================")
+print("luas lingkaran : pi x 2r, hasil =",pi*r*r,"cm")
+print("=======================")
